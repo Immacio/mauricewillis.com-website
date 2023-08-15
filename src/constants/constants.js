965550bdec27d3b1dfb21e60bf61d05e6bd6f67a
Maurice Willis <maurice@app-bar.com>
2023-08-15 19:15:27 -0400

    feat: Update projects section with doctor-booking-app

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -5,7 +5,7 @@ export const projects = [
       "An Instagram clone - a crypto-focused social media platform where users can like, comment and share photos about their favourite cryptocurrencies.",
     image: "/images/cryptogram.png",
     tags: ["Next.js", "NextAuth", "Firebase", "Recoil", "TailwindCSS"],
-    source: "https://cryptogram-seven.vercel.app/",
+    source: "https://cryptogram.mauricewillis.com/",
     id: 0,
   },
   {
@@ -23,7 +23,7 @@ export const projects = [
       "An NFT project developed using React and JavaScript. The collection was built and uploaded onto the Solana devnet, and allows users to connect their Phantom Wallet to mint NFTs natively on the site.",
     image: "/images/nft.png",
     tags: ["React", "Node", "JavaScript", "Solana"],
-    source: "https://bichonbuddies.vercel.app/",
+    source: "https://bichonbuddies.mauricewillis.com/",
     id: 2,
   },
   {
@@ -32,7 +32,7 @@ export const projects = [
       "A DAO project built on the Ethereum Rinkeby Testnet for audio lovers, developed using React and ThirdWeb API. Includes a native governance token, voting proposals, membership NFT and token minting.",
     image: "/images/audiodao.png",
     tags: ["React", "JavaScript", "ThirdWeb API", "Node"],
-    source: "https://audio-dao.vercel.app/",
+    source: "http://audio-dao.mauricewillis.com/",
     id: 3,
   },
   {
@@ -41,7 +41,16 @@ export const projects = [
       "An Airbnb clone - primarily a front-end project that utilizes TailwindCSS and Next.js SSR. Includes a working search functionality with a datepicker, that displays available rental homes with MapBox Integration.",
     image: "/images/notbnb.png",
     tags: ["React", "Next.js", "MapBox", "Node", "TailwindCSS"],
-    source: "https://notbnb-clone.vercel.app/",
+    source: "https://airbnbclone.mauricewillis.com/",
     id: 4,
+  },
+  {
+    title: "Bowtie Doctor Booking Page",
+    description:
+      "A doctor's booking page built using React. Connects to a back-end service that allows users to book appointments with a doctor, and displays the available time slots for each doctor.",
+    image: "/images/bowtie-doctor-booking.png",
+    tags: ["React", "React-Query", "TypeScript", "SCSS"],
+    source: "http://bowtie-booking-system.mauricewillis.com/",
+    id: 5,
   },
 ];
